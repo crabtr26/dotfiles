0766b1a661245b5c611798cdf29aaf64865ec5f2
jacob.sizzle <jacob@sizzle.gg>
2023-10-15 23:23:11 -0400

    Clean up fish shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
 # Fancy Prompt. Source - https://github.com/justjanne/powerline-go#bash
 function fish_prompt
-    eval $GOPATH/bin/powerline-go -error $status -jobs (count (jobs -p))
+    eval $GOPATH/bin/powerline-go -error $status -jobs (count (jobs -p)) -modules venv,cwd,git,hg,jobs,exit
 end
